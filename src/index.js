52025998a5e03818f4c3a09b1b90d65d4e930ce1
eapenzacharias <eapenzac@gmail.com>
2021-11-02 19:53:46 +0100

    Feat: Created html, scss, and updated index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,54 @@
-import _ from 'lodash';
 import './style/style.css';
 
-function component() {
-  const element = document.createElement('div');
+import { getElement, createElement } from './scripts/queries.js';
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const tasks = [
+  {
+    description: 'Lorem 1 ipsum dolor sit amet.',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Lorem 2 ipsum dolor sit amet.',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Lorem 3 ipsum dolor sit amet.',
+    completed: true,
+    index: 3,
+  },
+  {
+    description: 'Lorem 4 ipsum dolor sit amet.',
+    completed: false,
+    index: 4,
+  },
+  {
+    description: 'Lorem 5 ipsum dolor sit amet.',
+    completed: true,
+    index: 5,
+  },
+  {
+    description: 'Lorem 6 ipsum dolor sit amet.',
+    completed: false,
+    index: 6,
+  },
+];
 
-  return element;
+function printTasks(task) {
+  const li = createElement('li');
+  const done = createElement('input');
+  done.type = 'checkbox';
+  done.checked = task.completed;
+  const description = createElement('span');
+  description.innerHTML = task.description;
+  const menu = createElement('span');
+  menu.innerHTML = '⋮';
+  menu.className = 'task-options';
+  li.appendChild(done);
+  li.appendChild(description);
+  li.appendChild(menu);
+  getElement('#tasks').appendChild(li);
 }
 
-document.body.appendChild(component());
+tasks.forEach((task) => printTasks(task));
