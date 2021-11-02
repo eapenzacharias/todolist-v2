efa793dc7267902b51191b1e85054043debad088
eapenzacharias <eapenzac@gmail.com>
2021-11-03 00:47:35 +0100

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ module.exports = {
   entry: './src/index.js',
   mode: 'development',
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
